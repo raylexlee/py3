8c967cdef50c26fab75326716a23242e387ae46d
raylexlee <raylexym57lee@gmail.com>
2020-03-18 21:12:55 +0800

    Replace switch() with lambda conditional

--- 2019/QualificationRound/task2/task2.py
+++ 2019/QualificationRound/task2/task2.py
@@ -1,10 +1,6 @@
-def switch(w):
-    if w == 'E': return 'S'
-    return 'E'
-
 t = int(input())
 for i in range(1, t+1):
     n = int(input())
     p = input()
-    a = ''.join(map(lambda x : switch(x), list(p)))
+    a = ''.join(map(lambda x : 'E' if x == 'S' else 'S', list(p)))
     print("Case #{}: {}".format(i, a))
